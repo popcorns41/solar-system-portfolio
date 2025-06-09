287c625dc225ecd25efe73a1ace44ce5a4695211
popcorns41 <www.oliverhill123@gmail.com>
2025-06-09 21:33:22 +0100

    specify three js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.160.1/build/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
